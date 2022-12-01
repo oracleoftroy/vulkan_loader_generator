0f8f1587505e1460edc1056d1aa06c2d77d21fac
Marc Gallagher <marcgallagher@gmail.com>
2022-12-01 12:49:18 -0800

    aquire vcpkg with FetchContent

---
+++ cmake/vcpkg.cmake
@@ -0,0 +1,16 @@
+include(FetchContent)
+
+option(USE_VCPKG "Use vcpkg to provide dependencies" ON)
+
+if(${USE_VCPKG})
+	FetchContent_Declare(
+		vcpkg
+		GIT_REPOSITORY https://github.com/microsoft/vcpkg.git
+		GIT_TAG master
+		GIT_SHALLOW true
+		PATCH_COMMAND cmake -E rm "$<IF:${CMAKE_HOST_WIN32},vcpkg.exe,vcpkg>"
+	)
+
+	FetchContent_MakeAvailable(vcpkg)
+	include(${vcpkg_SOURCE_DIR}/scripts/buildsystems/vcpkg.cmake)
+endif()
