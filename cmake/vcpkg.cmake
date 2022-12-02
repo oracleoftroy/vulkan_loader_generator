ba9c7b5ff3b97cbfdc22bfddb05c34e40e8a411f
Marc Gallagher <marcgallagher@gmail.com>
2022-12-01 16:06:20 -0800

    check host system by name
    Apparently CMAKE_HOST_WIN32 is undefined on other systems, not false
    This aligns with the check vcpkg.cmake uses to determine the host system

--- cmake/vcpkg.cmake
+++ cmake/vcpkg.cmake
@@ -2,13 +2,16 @@ include(FetchContent)
 
 option(USE_VCPKG "Use vcpkg to provide dependencies" ON)
 
+set(HOST_IS_WINDOWS $<STREQUAL:${CMAKE_HOST_SYSTEM_NAME},Windows>)
+set(VCPKG_EXE_NAME $<IF:${HOST_IS_WINDOWS},vcpkg.exe,vcpkg>)
+
 if(${USE_VCPKG})
 	FetchContent_Declare(
 		vcpkg
 		GIT_REPOSITORY https://github.com/microsoft/vcpkg.git
 		GIT_TAG master
 		GIT_SHALLOW true
-		PATCH_COMMAND cmake -E rm -f "$<IF:${CMAKE_HOST_WIN32},vcpkg.exe,vcpkg>"
+		PATCH_COMMAND cmake -E rm -f ${VCPKG_EXE_NAME}
 	)
 
 	FetchContent_MakeAvailable(vcpkg)
